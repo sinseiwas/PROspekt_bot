7edb9c8a2567815915ca5b1bb90839503a4171a0
Stan <d-suhanuv@mail.ru>
2024-03-10 17:34:20 +0300

    added new buttons

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ from aiogram.filters import Command
 from aiogram.utils.formatting import (Bold, as_list, as_marked_section, as_key_value, HashTag)
 from aiogram.utils.markdown import hide_link
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 logging.basicConfig(level=logging.INFO)
@@ -19,7 +20,7 @@ bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
 dp = Dispatcher()
 dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
 
-@dp.message(Command("am_i_alone"))
+@dp.message(F.text.lower() == "am i alone")
 async def cmd_am_i_alone(message: Message):
     await message.answer(
         f"{hide_link('https://demotivatorium.ru/sstorage/3/2023/08/01101645749744/demotivatorium_ru_kak_ponjat_chto_vi_odinoki_220184.jpg')}"
@@ -27,22 +28,64 @@ async def cmd_am_i_alone(message: Message):
     )
 
 
+def get_keyboard():
+    buttons = [
+        [
+            types.InlineKeyboardButton(text="Режисёры", callback_data="director"),
+            types.InlineKeyboardButton(text="Организаторы", callback_data="organizers")
+        ],
+        [
+            types.InlineKeyboardButton(text="Дизайнеры", callback_data="dezigners"),
+            types.InlineKeyboardButton(text="СММ", callback_data="smm")
+        ],
+        [
+            types.InlineKeyboardButton(text="Куратор актёров", callback_data="actors"),
+            types.InlineKeyboardButton(text="Куратор командообразования", callback_data="comand_founder")
+        ]
+    ]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
+
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     builder = ReplyKeyboardBuilder()
     builder.add(types.KeyboardButton(text="bosses"))
-    builder.add(types.KeyboardButton(text="bot_founder"))
-    builder.add(types.KeyboardButton(text="in_development"))
+    builder.add(types.KeyboardButton(text="bot founder"))
+    builder.add(types.KeyboardButton(text="in development"))
     builder.add(types.KeyboardButton(text="hello"))
     builder.add(types.KeyboardButton(text="info"))
+    builder.add(types.KeyboardButton(text="am i alone"))
     builder.adjust(2)
     await message.answer(
         f"Привет! Театр ПРОспект приветствует тебя\n",
         reply_markup=builder.as_markup(resize_keyboard=True),
     )
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="ПРОспект", url="https://vk.com/teatr_prospekt")
+    )
+    await message.answer(
+        'Официальная группа вконтакте',
+        reply_markup=builder.as_markup(),
+    )
 
 
-@dp.message(F.text.lower() == "bot_founder")
+
+@dp.message(Command("random"))
+async def cmd_random(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Нажми меня",
+        callback_data="random_value")
+    )
+    await message.answer(
+        "Нажмите на кнопку, чтобы бот отправил число от 1 до 10",
+        reply_markup=builder.as_markup()
+    )
+
+
+@dp.message(F.text.lower() == "bot founder")
 async def cmd_hidden_link(message: Message):
     await message.answer(
         f"{hide_link('https://vk.com/worldpeacekeaper')}"
@@ -52,14 +95,49 @@ async def cmd_hidden_link(message: Message):
     )
 
 
-@dp.message(F.text.lower() == "in_development")
+@dp.message(F.text.lower() == "in development")
 async def cmd_in_development(message: types.Message):
     await message.answer("В планах добавить в бота должностные лица, расписание постановок, расписание смм, расписание треннингов")
 
 
 @dp.message(F.text.lower() == "bosses")
 async def cmd_bosses(message: types.Message):
-    await message.answer(text.prospekt_bosses)
+    await message.answer(
+        "О ком вы бы хотели узнать больше?", reply_markup=get_keyboard()
+    )
+
+
+@dp.callback_query(F.data == "director")
+async def send_director(callback: types.callback_query):
+    await callback.message.answer("Анастасия Переверзева")
+
+
+@dp.callback_query(F.data == "organizers")
+async def send_organizers(callback: types.callback_query):
+    await callback.message.answer("Олеся Крыжановская")
+
+
+@dp.callback_query(F.data == "dezigners")
+async def send_dezigners(callback: types.callback_query):
+    await callback.message.answer("Алина Шушкова")
+
+
+@dp.callback_query(F.data == "smm")
+async def send_smm(callback: types.callback_query):
+    await callback.message.answer("Дарья Гостева")
+
+
+@dp.callback_query(F.data == "actors")
+async def send_actors(callback: types.callback_query):
+    await callback.message.answer("Ася Панина")
+
+
+@dp.callback_query(F.data == "comand_founder")
+async def send_comand_founder(callback: types.callback_query):
+    await callback.message.answer(
+        f"{hide_link('https://www.meme-arsenal.com/memes/a55ad4ef8fc0f03e7c0feedb538fb736.jpg')}"
+        f"Иван Зазулин"
+    )
 
 
 @dp.message(F.text.lower() == "hello")
